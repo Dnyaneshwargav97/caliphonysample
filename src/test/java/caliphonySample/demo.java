3dbfada3b35c1f0449206547d1f6d4f0dd330929
Dnyaneshwargav97 <dnyaneshwargavade@gmail.com>
2022-12-14 17:07:09 +0530

    first commit to github

---
+++ src/test/java/caliphonySample/demo.java
@@ -0,0 +1,12 @@
+package caliphonySample;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello from caliphony sample");
+
+	}
+
+}
